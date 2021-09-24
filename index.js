2a609a1faff3b26120af7d12d4ce0df458fbe0aa
EhWhatsThePoint <69695848+EhWhatsThePoint@users.noreply.github.com>
2021-09-24 12:11:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
+const addtoserver = express();
+const about = express();
+const source = express();
+const documentation = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Express app!')
+  res.sendFile(__dirname + '/index.html');
 });
+addtoserver.get('/', (req, res) => {
+  res.sendFile('/ats.html');
+});
+about.get('/', (req, res) => {
+  res.sendFile('/about.html');
+});
+source.get('/', (req, res) => {
+  res.sendFile('/source.html');
+});
+documentation.get('/', (req, res) => {
+  res.sendFile('/documentation.html')
+});
+
+app.use("/about", addtoserver);
+app.use("/source", addtoserver);
+app.use("/documentation", addtoserver);
+app.use("/addtoserver", addtoserver);
 
 app.listen(3000, () => {
   console.log('server started');
