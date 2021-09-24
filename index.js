708c94d2fadc6b594680c40623f0f8b99062de55
EhWhatsThePoint <69695848+EhWhatsThePoint@users.noreply.github.com>
2021-09-24 11:57:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const express = require("Express");
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Express app!')
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});
